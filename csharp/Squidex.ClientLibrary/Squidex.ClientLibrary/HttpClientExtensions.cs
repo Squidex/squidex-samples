56af74d127085f298c69fdd06ce2aa74ed9451f9
Sebastian Stehle <sebastian@squidex.io>
2018-09-07 17:49:34 +0200

    Keep casing.

--- csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/HttpClientExtensions.cs
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/HttpClientExtensions.cs
@@ -6,6 +6,7 @@
 // ==========================================================================
 
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -15,14 +16,20 @@ namespace Squidex.ClientLibrary
 {
     public static class HttpClientExtensions
     {
-        private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
+        private static readonly JsonSerializerSettings PascalCasing = new JsonSerializerSettings();
+        private static readonly JsonSerializerSettings CamelCasing = new JsonSerializerSettings
         {
             ContractResolver = new CamelCasePropertyNamesContractResolver()
         };
 
         public static HttpContent ToContent<T>(this T value)
         {
-            var content = new StringContent(JsonConvert.SerializeObject(value, Formatting.Indented, SerializerSettings), Encoding.UTF8, "application/json");
+            var serializerSettings =
+                value.GetType().GetCustomAttribute<KeepCasingAttribute>() != null ?
+                    PascalCasing :
+                    CamelCasing;
+
+            var content = new StringContent(JsonConvert.SerializeObject(value, Formatting.Indented, serializerSettings), Encoding.UTF8, "application/json");
 
             return content;
         }
---
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/KeepCasingAttribute.cs
@@ -0,0 +1,16 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System;
+
+namespace Squidex.ClientLibrary
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public sealed class KeepCasingAttribute : Attribute
+    {
+    }
+}
