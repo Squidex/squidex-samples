fd1bd57a637d16cc894b4eba9d1cb6d3d23be4fc
Sebastian Stehle <sebastian@squidex.io>
2019-07-26 14:37:47 +0200

    Convert to utc.

--- csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/HttpClientExtensions.cs
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/HttpClientExtensions.cs
@@ -5,33 +5,55 @@
 //  All rights reserved. Licensed under the MIT license.
 // ==========================================================================
 
+using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
+using Squidex.ClientLibrary.Utils;
 
 namespace Squidex.ClientLibrary
 {
     public static class HttpClientExtensions
     {
-        private static readonly JsonSerializerSettings PascalCasing = new JsonSerializerSettings();
-        private static readonly JsonSerializerSettings CamelCasing = new JsonSerializerSettings
+        private static readonly JsonSerializerSettings PascalCasing = CreateSerializer(new DefaultContractResolver());
+
+        private static readonly JsonSerializerSettings CamelCasing = CreateSerializer(new CamelCasePropertyNamesContractResolver());
+
+        private static JsonSerializerSettings CreateSerializer(DefaultContractResolver contractResolver)
         {
-            ContractResolver = new CamelCasePropertyNamesContractResolver()
-        };
+            var result = new JsonSerializerSettings
+            {
+                ContractResolver = contractResolver
+            };
+
+            result.Converters.Add(new UTCIsoDateTimeConverter());
+
+            return result;
+        }
 
         public static HttpContent ToContent<T>(this T value)
+        {
+            var json = value.ToJson();
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return content;
+        }
+
+        public static string ToJson<T>(this T value)
         {
             var serializerSettings =
                 value.GetType().GetCustomAttribute<KeepCasingAttribute>() != null ?
                     PascalCasing :
                     CamelCasing;
 
-            var content = new StringContent(JsonConvert.SerializeObject(value, Formatting.Indented, serializerSettings), Encoding.UTF8, "application/json");
+            var json = JsonConvert.SerializeObject(value, Formatting.Indented, serializerSettings);
 
-            return content;
+            return json;
         }
 
         public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
