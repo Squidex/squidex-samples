4c5a881dd74d141fdb343c106246cabc425825dd
Erek Röös <erek.roeoes@coduct.com>
2020-05-13 09:07:12 +0200

    chore: style policy

--- csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/ISquidexClientManager.cs
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/ISquidexClientManager.cs
@@ -14,22 +14,39 @@ namespace Squidex.ClientLibrary
     public interface ISquidexClientManager
     {
         SquidexOptions Options { get; }
+
         string GenerateImageUrl(string id);
+
         string GenerateImageUrl(IEnumerable<string> id);
+
         IAppsClient CreateAppsClient();
+
         IAssetsClient CreateAssetsClient();
+
         IBackupsClient CreateBackupsClient();
+
         ICommentsClient CreateCommentsClient();
+
         IHistoryClient CreateHistoryClient();
+
         ILanguagesClient CreateLanguagesClient();
+
         IPingClient CreatePingClient();
+
         IPlansClient CreatePlansClient();
+
         IRulesClient CreateRulesClient();
+
         ISchemasClient CreateSchemasClient();
+
         IStatisticsClient CreateStatisticsClient();
+
         IUsersClient CreateUsersClient();
+
         IExtendableRulesClient CreateExtendableRulesClient();
+
         IContentsClient<TEntity, TData> CreateContentsClient<TEntity, TData>(string schemaName) where TEntity : Content<TData> where TData : class, new();
+
         HttpClient CreateHttpClient();
     }
 }
