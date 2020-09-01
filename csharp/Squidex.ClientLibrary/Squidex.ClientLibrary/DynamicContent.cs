0a377c768fe97b48d69ffe45606244d2704bca7f
Sebastian <sebastian@squidex.io>
2020-09-01 13:47:17 +0200

    Make dynamic data more dynamic

--- csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/DynamicContent.cs
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/DynamicContent.cs
@@ -5,17 +5,9 @@
 //  All rights reserved. Licensed under the MIT license.
 // ==========================================================================
 
-using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
-
 namespace Squidex.ClientLibrary
 {
     public sealed class DynamicContent : Content<DynamicData>
-    {
-    }
-
-    [KeepCasing]
-    public sealed class DynamicData : Dictionary<string, JObject>
     {
     }
 }
