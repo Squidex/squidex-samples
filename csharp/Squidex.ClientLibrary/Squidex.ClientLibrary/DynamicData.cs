2cc2df71ffa39d23834f824c76ee4d4c5c7389ff
Sebastian <sebastian@squidex.io>
2020-09-01 13:47:24 +0200

    Missing file

---
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/DynamicData.cs
@@ -0,0 +1,17 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Squidex.ClientLibrary
+{
+    [KeepCasing]
+    public sealed class DynamicData : Dictionary<string, JObject>
+    {
+    }
+}
