b6644567739211d236a4d798b5a6f8be9ff7ece9
Sebastian <sebastian@squidex.io>
2022-03-27 18:46:34 +0200

    Increase CLI version.

---
+++ cli/Squidex.CLI/Squidex.CLI.Core/Commands/Implementation/Sync/App/AssetScriptsModel.cs
@@ -0,0 +1,22 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+namespace Squidex.CLI.Commands.Implementation.Sync.App
+{
+    public sealed class AssetScriptsModel
+    {
+        public string? Create { get; set; }
+
+        public string? Update { get; set; }
+
+        public string? Annotate { get; set; }
+
+        public string? Move { get; set; }
+
+        public string? Delete { get; set; }
+    }
+}
