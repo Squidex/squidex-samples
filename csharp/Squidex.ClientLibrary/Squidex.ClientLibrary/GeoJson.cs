e01e5c0c2e56ff0162ecb261699f48504626e560
Sebastian <sebastian@squidex.io>
2022-04-23 18:14:29 +0200

    Add missing files.

---
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/GeoJson.cs
@@ -0,0 +1,42 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+namespace Squidex.ClientLibrary
+{
+    /// <summary>
+    /// GeoJSON helper.
+    /// </summary>
+    public static class GeoJson
+    {
+        /// <summary>
+        /// Creates a new geo-json point.
+        /// </summary>
+        /// <param name="longitude">The longitude.</param>
+        /// <param name="latitude">The latitude.</param>
+        /// <param name="oldFormat">Uses the old format.</param>
+        /// <returns>
+        /// The geojson object.
+        /// </returns>
+        public static object Point(double longitude, double latitude, bool oldFormat = false)
+        {
+            if (oldFormat)
+            {
+                return new { longitude, latitude };
+            }
+
+            return new
+            {
+                type = "Point",
+                coordinates = new[]
+                {
+                    longitude,
+                    latitude,
+                }
+            };
+        }
+    }
+}
