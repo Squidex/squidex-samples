1e4ce53c5280e1e231cbced2f71532949f0ddfec
Sebastian <sebastian@squidex.io>
2022-11-07 15:49:35 +0100

    Add missing files.

---
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/Configuration/Cache.cs
@@ -0,0 +1,100 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+namespace Squidex.ClientLibrary.Configuration
+{
+    internal sealed class Cache<TKey, TValue>
+    {
+        private readonly Dictionary<TKey, CacheEntry> items = new Dictionary<TKey, CacheEntry>();
+
+        private sealed class CacheEntry
+        {
+            public TValue Value { get; set; }
+
+            public DateTimeOffset Expires { get; set; }
+        }
+
+        public bool Remove(TKey key)
+        {
+            lock (items)
+            {
+                Cleanup();
+
+                return items.Remove(key);
+            }
+        }
+
+        public void Set(TKey key, TValue value, DateTimeOffset expires)
+        {
+            lock (items)
+            {
+                Cleanup();
+
+                items[key] = new CacheEntry { Value = value, Expires = expires };
+            }
+        }
+
+        public bool TryGet(TKey key, out TValue value)
+        {
+            lock (items)
+            {
+                Cleanup();
+
+                value = default!;
+
+                if (items.TryGetValue(key, out var entry))
+                {
+                    value = entry.Value;
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public void Set(TKey key, TValue value, TimeSpan expires)
+        {
+            if (expires < TimeSpan.Zero || expires > TimeSpan.FromDays(365))
+            {
+                return;
+            }
+
+            Set(key, value, DateTime.UtcNow + expires);
+        }
+
+        private void Cleanup()
+        {
+            if (items.Count == 0)
+            {
+                return;
+            }
+
+            List<TKey>? keysToRemove = null;
+
+            var now = DateTime.UtcNow;
+
+            foreach (var kvp in items)
+            {
+                if (kvp.Value.Expires <= now)
+                {
+                    keysToRemove ??= new List<TKey>();
+                    keysToRemove.Add(kvp.Key);
+                }
+            }
+
+            if (keysToRemove == null)
+            {
+                return;
+            }
+
+            foreach (var key in keysToRemove)
+            {
+                items.Remove(key);
+            }
+        }
+    }
+}
---
+++ csharp/Squidex.ClientLibrary/Squidex.ClientLibrary/OptionsBase.cs
@@ -0,0 +1,50 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+namespace Squidex.ClientLibrary
+{
+    /// <summary>
+    /// Base class for options.
+    /// </summary>
+    public abstract class OptionsBase
+    {
+        private bool isFrozen;
+
+        /// <summary>
+        /// Indicates whether the options are frozen.
+        /// </summary>
+        public bool IsFrozen => isFrozen;
+
+        /// <summary>
+        /// Sets a value if not frozen.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="target">The target field.</param>
+        /// <param name="value">The value.</param>
+        protected void Set<T>(ref T target, T value)
+        {
+            ThrowIfFrozen();
+            target = value;
+        }
+
+        /// <summary>
+        /// Frezes the options.
+        /// </summary>
+        protected void Freeze()
+        {
+            isFrozen = true;
+        }
+
+        private void ThrowIfFrozen()
+        {
+            if (isFrozen)
+            {
+                throw new InvalidOperationException("Options are frozen and cannot be changed.");
+            }
+        }
+    }
+}
